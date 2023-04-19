e0d2ef2f8e33d757ef1e7bbd52cec556fb896446
mroziken <michal.mrozik@gmail.com>
2023-04-19 22:15:57 +0000

    assignment

--- contract/tictactoe.cpp
+++ contract/tictactoe.cpp
@@ -6,9 +6,43 @@ CONTRACT tictactoe : public contract {
   public:
     using contract::contract;
 
+    TABLE game{
+      name host;
+      name challenger;
+
+      uint64_t primary_key() const { return host.value; }
+    };
+    typedef multi_index<"games"_n, game> games;
+
+
     ACTION welcome(name host, name challenger){
       require_auth(get_self());
       print("Welcome, challengers ", host," and ",challenger,"!");
+    }
+
+    ACTION create( const name &challenger, name &host ){
+      require_auth(host);
+
+      games existing_games(get_self(), get_self().value);
+      auto game_iterator = existing_games.find(host.value);
+
+      check(game_iterator == existing_games.end(), "Game already exists between host and challenger.");
+
+      existing_games.emplace(get_self(), [&](auto& new_game) {
+          new_game.host = host;
+          new_game.challenger = challenger;
+      });
+    }
+
+    ACTION close(name challenger, name host){
+      require_auth(host);
+
+      games existing_games(get_self(),get_self().value);
+      auto game_iterator = existing_games.find(host.value);
+
+      check(game_iterator != existing_games.end(), "Game does not exist!");
+
+      existing_games.erase(game_iterator);
     }
 
     private:
--- contract/toyhouse.cpp
+++ contract/toyhouse.cpp
@@ -18,7 +18,7 @@ CONTRACT toyhouse : public contract {
     };
     typedef multi_index<name("toyshelf"), toy> toyshelf_table;
 
-    ACTION addtoy(int64_t id, string toyname, string toydescrip, uint64_t quantity){
+    ACTION addtoy(int128_t id, string toyname, string toydescrip, uint64_t quantity){
       check(has_auth(name("boss")), "You are not the boss");
       toyshelf_table _toyshelf(get_self(), get_self().value);
       // add the new toy if it does not exist
@@ -39,14 +39,16 @@ CONTRACT toyhouse : public contract {
       }
     }
 
-    ACTION removetoy( int64_t id, uint64_t quantity){
+    ACTION removetoy( uint64_t id, uint64_t quantity){
       require_auth(name("boss"));
       toyshelf_table _toyshelf(get_self(), get_self().value);
       //find toy
       auto itr = _toyshelf.find(id);
       //if does not exists, error
+      //print("itr=", itr);
       check(itr != _toyshelf.end(),"toy with given id not found");
-      print("itr=", itr, "itr->quantity=", itr->quantity);
+      //print("itr=", itr, "itr->quantity=", itr->quantity);
+      
       if((itr->quantity - quantity) == 0){
         //if resulting quantity is 0, delete toy row
         _toyshelf.erase(itr);
@@ -54,12 +56,18 @@ CONTRACT toyhouse : public contract {
       }
       else{
         //if resulting quantity is <0, error
-        check((itr->quantity - quantity) > 0, "You are trying to have negative toys");
-        //substract quantity
-        _toyshelf.modify(itr, get_self(), [&](auto& row_to_modify){
-          row_to_modify.quantity -= quantity;
-        });
+        //check( (itr->quantity - quantity) > 0, "You are trying to have negative toys");
+        if(itr->quantity < quantity){
+          print("You are trying to have negative toys");
+        }
+        else{
+          //substract quantity
+          _toyshelf.modify(itr, get_self(), [&](auto& row_to_modify){
+            row_to_modify.quantity -= quantity;
+          });
+        }
       }
+      
     }
 
     private:

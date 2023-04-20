36f48db1a33a9c7e92c15adc23e4dd54ab45f897
mroziken <michal.mrozik@gmail.com>
2023-04-20 20:28:32 +0000

    adding secondary index

--- contract/tictactoe.cpp
+++ contract/tictactoe.cpp
@@ -11,16 +11,18 @@ CONTRACT tictactoe : public contract {
       name challenger;
 
       uint64_t primary_key() const { return host.value; }
+      uint64_t by_challenger() const {return challenger.value;}
     };
-    typedef multi_index<"games"_n, game> games;
-
+    typedef multi_index<"games"_n, game,
+    indexed_by<"bychallenger"_n, const_mem_fun<game, uint64_t, &game::by_challenger>>> games;
+ 
 
     ACTION welcome(name host, name challenger){
       require_auth(get_self());
       print("Welcome, challengers ", host," and ",challenger,"!");
     }
 
-    ACTION create( const name &challenger, name &host ){
+    ACTION create( name &challenger, name &host ){
       require_auth(host);
 
       games existing_games(get_self(), get_self().value);
@@ -32,6 +34,21 @@ CONTRACT tictactoe : public contract {
           new_game.host = host;
           new_game.challenger = challenger;
       });
+    }
+
+    ACTION findgames(name &challenger){
+      games games_by_challenger(get_self(), get_self().value);
+      auto game_iterator = games_by_challenger.lower_bound(challenger.value);
+      auto game_iterator_term = games_by_challenger.upper_bound(challenger.value);
+      while (game_iterator != game_iterator_term){
+        if(game_iterator->challenger == challenger){
+          print("Game between ",game_iterator->host, " and ", game_iterator->challenger, "\n");
+        }
+        else{
+          print("xxx = ", game_iterator->challenger);
+        }
+        ++game_iterator;
+      }
     }
 
     ACTION close(name challenger, name host){
@@ -47,3 +64,5 @@ CONTRACT tictactoe : public contract {
 
     private:
 };
+
+EOSIO_DISPATCH(tictactoe, (welcome)(create)(close)(findgames))
